ab41794372f034c1680ffe686df70416695c1a8a
Luis Iván García Luna <Luis Ivn Garca Luna>
2016-10-04 13:56:12 -0500

    Martes 04 de Oct

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ $nombre= "Luis Iván";
+ $numero= 2+2;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba PHP</title>
+  </head>
+  <body>
+    <h1>Hola <?php echo $nombre ?></h1>
+    <h2>Número<?php echo $numero ?></h2>
+  </body>
+</html>
